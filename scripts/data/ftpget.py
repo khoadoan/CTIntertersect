21e7e8a0bcc852d120346c9ba07a4a3ec008e9d9
Khoa Doan <kdoan1@mas-nn.localdomain>
2013-07-09 17:04:54 -0400

    Add FTP utility in Python for downloading contents  Please enter the commit message for your changes. Lines starting

---
+++ scripts/data/ftpget.py
@@ -0,0 +1,25 @@
+import os
+from ftplib import FTP
+import sys
+
+host = sys.argv[1];
+remote_dir = sys.argv[2];
+local_dir = sys.argv[3];
+ext = sys.argv[4];
+
+ftp = FTP(host)
+ftp.login()
+
+ftp.cwd(remote_dir)
+
+listing = []
+ftp.retrlines("LIST", listing.append)
+filenames = [item.split(None, 8)[-1].lstrip() for item in listing]
+filenames = filter(lambda filename: filename.lower().endswith(ext.lower()), filenames)
+
+for filename in filenames:
+    print('FTP ' + filename)
+    local_filename = os.path.join(local_dir, filename)
+    lf = open(local_filename, "wb")
+    ftp.retrbinary("RETR " + filename, lf.write, 8*1024)
+    lf.close()
