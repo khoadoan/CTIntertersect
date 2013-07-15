e1dbd121d8d0e9ff443d704671ffd637cd00bb37
Khoa Doan <kdoan1@mas-nn.localdomain>
2013-07-14 22:48:43 -0400

    Add running script to insert data into scidb

--- scidb/scripts/insert_cloudsat.sh
+++ scidb/scripts/insert_cloudsat.sh
@@ -8,13 +8,15 @@ MONTH=$4
 WORKING_DIR=$5
 CONVERT_SCRIPT_DIR=$6
 MATLAB_MCR_DIR=$7
+COMMON_SCRIPT_DIR=$8
+
 CINTDATA="/user/kdoan1/CInt/data"
 
 # Intialization
-
-WORKING_DIR_HDF="$WORKING_DIR/hdf"
-WORKING_DIR_CSV="$WORKING_DIR/csv"
-WORKING_DIR_SCIDB="$WORKING_DIR/scidb"
+WORKING_DIR_BASE="$WORKING_DIR/cloudsat"
+WORKING_DIR_HDF="$WORKING_DIR_BASE/hdf"
+WORKING_DIR_CSV="$WORKING_DIR_BASE/csv"
+WORKING_DIR_SCIDB="$WORKING_DIR_BASE/scidb"
 
 if [ ! -e $WORKING_DIR_HDF ] ; then
     /bin/mkdir -p $WORKING_DIR_HDF
@@ -28,6 +30,8 @@ if [ ! -e $WORKING_DIR_SCIDB ] ; then
     /bin/mkdir -p $WORKING_DIR_SCIDB
 fi
 
+. $COMMON_SCRIPT_DIR/functions.sh
+
 # Figure out the days of the year for the starting day and ending day of
 # the requested month, since TRMM data is organized in day-of-the-year
 # directory at the source FTP site.. 
@@ -36,7 +40,7 @@ DEVE=`printf "12/31/%d" "$YEAR"`
 
 #     Get the number of days in the year.
 get_day_of_year $DEVE
-NDAY=$__JDOY# __JDOY is set in get_day_of_year function.
+NDAY=$__JDOY # __JDOY is set in get_day_of_year function.
 
 #     Get the first day of the month.
 DBGN=`printf "%d/1/%d" "$MONTH" "$YEAR"`
@@ -45,10 +49,10 @@ DBGN=`printf "%d/1/%d" "$MONTH" "$YEAR"`
 let "NMON=$MONTH+1"
 
 #     Increment the year if the following month number is greater than 12.
-let "YINC=0"
+YINC=0
 if [ $NMON -gt 12 ] ; then
     let "NMON=$NMON%12"
-    let "YINC=$MONTH/12"
+    let "YINC=$NMON/12"
 fi
 let "NYR=$YEAR+$YINC"
 
@@ -67,22 +71,23 @@ if [ $JEND -lt $JBGN ] ; then
 fi
 
 for ((DOY=$JBGN; DOY<$JEND; DOY++)); do
-	HDFSFILES=`printf '%s/CloudSat/%s/%03d/%s/*.hdf.zip' "$CINTDATA" "$YEAR" "$DOY" "$RELEASE"`
-    
+	HDFSFILES=`printf '%s/CloudSat/%s/%s/%03d/%s/*.hdf.zip' "$CINTDATA" "$PROD" "$YEAR" "$DOY" "$RELEASE"`
+
 	# download hdf files from HDFS
+	echo "...Download files from $HDFFILES"
 	cd "$WORKING_DIR_HDF"
-    hadoop fs -get "$HDFSFILES"
+	hadoop fs -get "$HDFSFILES"
 	
 	# uncompress
 	for COMPRSD in *.hdf.zip
-    do
+	do
 		FINDEX=`expr length $COMPRSD - 4` 
 		HDFFNAME=`expr substr $COMPRSD 1 $FINDEX`
 		printf "Extracting %s to %s\n" $COMPRSD $HDFFNAME
-		unzip $COMPRSD
+		unzip -o $COMPRSD
 	
 		# Convert hdf files to csv
-		"$CONVERT_SCRIPT_DIR/run_trmm.sh" "$MATLAB_MCR_DIR"
+		"$CONVERT_SCRIPT_DIR/run_cloudsat.sh" "$MATLAB_MCR_DIR"
 		
 		FINDEX=`expr length $HDFFNAME - 4` 
 		FNAME=`expr substr $COMPRSD 1 $FINDEX`
@@ -106,7 +111,8 @@ for ((DOY=$JBGN; DOY<$JEND; DOY++)); do
 		/bin/rm -f "$HDFFNAME"
 		/bin/rm -f "$CSVFILE"
 		/bin/rm -f "$SCIDBFILE"
-    done
-
-    
+	done
 done
+
+# Remove CloudSat Working Dir Base
+/bin/rm -r -f "$WORKING_DIR_BASE"
